83bb02131f15de3c9af0b304e7063f921e82ffce
Spencer Murray <spencer@spalmurray.com>
2023-08-23 00:07:29 -0700

    Auto install packer

--- .config/nvim/lua/spalmurray/packer.lua
+++ .config/nvim/lua/spalmurray/packer.lua
@@ -1,5 +1,18 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
@@ -47,4 +60,8 @@ return require('packer').startup(function(use)
     use('puremourning/vimspector')
     use('airblade/vim-gitgutter')
     use('lukas-reineke/indent-blankline.nvim')
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
