3743aad162f41505caef48de01c734b824dc37ca
Spencer Murray <spencer@spalmurray.com>
2023-09-07 12:18:45 -0400

    macos bad

--- .config/nvim/after/plugin/vimspector.lua
+++ .config/nvim/after/plugin/vimspector.lua
@@ -1,8 +1,6 @@
-vim.g.vimspector_install_gadgets = { 'vscode-cpptools', 'debugpy', 'vscode-bash-debug', 'vscode-js-debug', 'vscode-node-debug2' }
-
 vim.g.vimspector_sidebar_width = 80
 vim.g.vimspector_bottombar_height = 20
-vim.g.vimspector_base_dir = '/Users/spencer/.local/share/nvim/site/pack/packer/start/vimspector'
+vim.g.vimspector_base_dir = '$HOME/.local/share/nvim/site/pack/packer/start/vimspector'
 
 vim.keymap.set("n", "<F5>", ":call vimspector#Continue()<CR>")
 vim.keymap.set("n", "<F6>", ":call vimspector#Restart()<CR>")
