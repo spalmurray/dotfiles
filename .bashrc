1a1313e7a909c0225e5ad4b96306a25f0a82850f
Spencer Murray <spencer.murray@sentry.io>
2024-02-15 15:54:13 -0500

    buncha work config changes

--- .bashrc
+++ .bashrc
@@ -2,6 +2,15 @@
 # ~/.bashrc
 #
 
+# macos stuff
+export PATH="/opt/homebrew/bin:$PATH"
+alias python='python3'
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+export BASH_SILENCE_DEPRECATION_WARNING=1
+export SHELL="/opt/homebrew/bin/bash"
+
 # Bash history config
 export HISTSIZE=
 export HISTFILESIZE=
@@ -9,11 +18,14 @@ export HISTFILESIZE=
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
 
-export PS1="\[\e[31m\]\u\]\e[37m\]@\e[33m\]\h\e[37m\]:\e[32m\]\w \e[37m\]\n\\$ "
+export PS1="\[\e[31m\]\u\]\e[37m\]@\e[33m\]work\e[37m\]:\e[32m\]\w \e[37m\]\n\\$ "
 
 # GPG
 export GPG_TTY=$(tty)
-export HOME=/home/spencer
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
+gpg-connect-agent updatestartuptty /bye > /dev/null
+export HOME=/Users/spencer
 
 # Aliases
 alias vim=nvim
@@ -32,24 +44,10 @@ alias gu='CURRENT_GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD) && git checkout 
 
 # Exports
 export PATH=$PATH:~/bin/
-export PATH=$PATH:/home/spencer/.local/bin
+export PATH=$PATH:"$HOME.local/bin"
 export SUDO_EDITOR=vim
 export EDITOR=vim
-export PICO_SDK_PATH=/home/spencer/Documents/projects/pico/pico-sdk
-export BROWSER=/usr/bin/librewolf
-export XDG_CONFIG_HOME=/home/spencer/.config
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-if test -z "${XDG_RUNTIME_DIR}"; then
-    export XDG_RUNTIME_DIR=/tmp/spencers-runtime-dir
-    if ! test -d "${XDG_RUNTIME_DIR}"; then
-        mkdir "${XDG_RUNTIME_DIR}"
-        chmod 0700 "${XDG_RUNTIME_DIR}"
-    fi
-fi
+export XDG_CONFIG_HOME="$HOME/.config"
 
 # Use vim binds
 set -o vi
@@ -63,10 +61,3 @@ eval "$(zoxide init bash)"
 export FZF_DEFAULT_COMMAND='find ~'
 export FZF_DEFAULT_OPTS='--border'
 
-# gpg for ssh
-unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-fi
-export GPG_TTY=$(tty)
-gpg-connect-agent updatestartuptty /bye >/dev/null
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/spencer/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/spencer/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/spencer/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/spencer/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/spencer/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/spencer/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?.lua;/Users/spencer/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?/init.lua;/Users/spencer/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?.lua;/Users/spencer/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/spencer/.cache/nvim/packer_hererocks/2.1.1703358377/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,89 +76,89 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["gruvbox-material"] = {
     config = { "\27LJ\2\n@\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0!colorscheme gruvbox-material\bcmd\bvim\0" },
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/gruvbox-material",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/gruvbox-material",
     url = "https://github.com/sainnhe/gruvbox-material"
   },
   harpoon = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/harpoon",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/theprimeagen/harpoon"
   },
   ["indent-blankline.nvim"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-autopairs"] = {
     config = { "\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["vim-gitgutter"] = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/vim-gitgutter",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/vim-gitgutter",
     url = "https://github.com/airblade/vim-gitgutter"
   },
   vimspector = {
     loaded = true,
-    path = "/home/spencer/.local/share/nvim/site/pack/packer/start/vimspector",
+    path = "/Users/spencer/.local/share/nvim/site/pack/packer/start/vimspector",
     url = "https://github.com/puremourning/vimspector"
   }
 }
