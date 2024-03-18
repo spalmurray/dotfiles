3402bc35b0cd7093c705ea2d5bad6a8fa2ef9e07
Spencer Murray <spencer.murray@sentry.io>
2024-03-18 14:43:39 -0400

    Remove linux stuff

--- .config/sway/on_sway_load.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# Make sure clamshell mode stays on after sway reload
-if grep -q open /proc/acpi/button/lid/LID/state; then
-	swaymsg output eDP-1 enable
-else
-	swaymsg output eDP-1 disable
-fi
