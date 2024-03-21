eec29a4c6f2da4dbd1b9287d2be298810bb66d0d
Spencer Murray <spencer@spalmurray.com>
2024-03-20 20:53:45 -0400

    Bring over macos nvim config

--- .config/nvim/lua/spalmurray/set.lua
+++ .config/nvim/lua/spalmurray/set.lua
@@ -7,7 +7,7 @@ vim.g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 
 -- undos
