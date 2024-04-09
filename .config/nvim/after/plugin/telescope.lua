3efce661eaf79033103de369aa5dfb9c9e20a34c
Spencer Murray <spencer.murray@sentry.io>
2024-04-09 12:27:54 -0400

    Truncate telescope lines

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,7 +2,11 @@ require('telescope').setup{
     defaults = {
         file_ignore_patterns = {
             ".git/",
-            "node_modules/"
+            "node_modules/",
+            ".git/"
+        },
+        path_display = {
+            "truncate"
         }
     },
     pickers = {
