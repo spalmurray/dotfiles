753553ff909d839017de44429a75b62f465b3426
Spencer Murray <spencer.murray@sentry.io>
2024-04-11 15:22:19 -0400

    Telescope no index.js/ts and control q passthrough to alacritty

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,9 @@
 require('telescope').setup{
     defaults = {
         file_ignore_patterns = {
-            ".git/"
+            ".git/",
+            "index.ts",
+            "index.js"
         },
         path_display = {
             "truncate"
